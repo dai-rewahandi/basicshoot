ccb5c750a58fbf3ee38608563e6cd4c5bf06667b
dai-rewhandi <niktodaire@gmail.com>
2025-04-15 13:23:03 +0700

    Refactor image processing script to modular functions and remove unused files

--- app.sh
+++ app.sh
@@ -1,101 +1,104 @@
 #!/bin/sh
 clear
 
+config_file=~/.config/bs/config.sh
 WATCH_DIR="$HOME/Pictures/Screenshots"
-echo "Watching Screenshots: $WATCH_DIR"
+FONT="$HOME/.config/bs/font.ttf"
+
+function check_fonfig() { 
+  if [ ! -f "$config_file" ]; then
+    echo "config file not found!"
+    exit 1
+  fi
+}
+
+check_fonfig
+source $config_file
 
 inotifywait -e create --format '%f' "$WATCH_DIR" | while read NEWFILE ; do
-  # check if file is an image
   if [ -f "$WATCH_DIR/$NEWFILE" ]; then
-    echo "File is an image! Processing... $NEWFILE"
     NEWNAME=$NEWFILE
     NEWFILE=$WATCH_DIR/$NEWFILE
-
-    # check if config file exists
-    config_file=~/.config/bs/config.sh
-    if [ ! -f "$config_file" ]; then
-      echo "config file not found!"
-      exit 1
-    fi
-
-    # check size of image
     size=$( identify -format "%w %h" $NEWFILE )
     arr=($size)
-    width=${arr[0]} # width of image
-    height=${arr[1]} # height of image
 
-    # width and height of background ( width + 200, height + 200 )
+    width=${arr[0]}
+    height=${arr[1]}
+
     width_bg=$(( ${arr[0]} + 200 ))
     height_bg=$(( ${arr[1]} + 200 ))
 
-    # rounded corners
     border_width=$((width + 4))
     border_height=$((height + 4))
-
-    # offset for corners
+    
     offset_x=$(( (width_bg - width)))
     offset_y=$(( (height_bg - height)))
 
-    # define radius for rounded corners
-    radius=20
+    function Make_image_with_border_rounded() {
+      echo "Adding rounded corners..."
 
-    # make rounded corners
-    magick -size ${width}x${height} xc:none \
-      -fill white -draw "roundrectangle 0,0 $((width-1)),$((height-1)) $radius,$radius" \
-      border_redius_mask.png
+      magick -size ${width}x${height} xc:none \
+        -fill white -draw "roundrectangle 0,0 $((width-1)),$((height-1)) $radius,$radius" \
+        border_redius_mask.png
+      magick $NEWFILE \( border_redius_mask.png -alpha set \) -compose DstIn -composite rounded_image.png
+    }
+    function Make_shadow() {
+      echo "Adding shadows..."
 
-    # make border mask
-    # magick -size ${border_width}x${border_height} xc:none \
-    #   -fill "rgb(129, 129, 129)" -draw "roundrectangle 0,0 $((border_width-1)),$((border_height-1)) $radius,$radius" \
-    #   border_mask.png
+      magick rounded_image.png \
+        \( +clone -background "rgb(0, 0, 0)" -shadow 60x7+3+3 \) \
+        +swap -background none -layers merge +repage \
+        tmp_shadow1.png
+      magick tmp_shadow1.png \
+        \( +clone -background "rgb(0, 0, 0)" -shadow 60x7-3+3 \) \
+        +swap -background none -layers merge +repage \
+        rounded_image_with_boder_shadowed.png
+    }
 
-    # make rounded image
-    magick $NEWFILE \( border_redius_mask.png -alpha set \) -compose DstIn -composite rounded_image.png
+    function Make_background() {
+      echo "Making Btfl background..."
 
-    # add border to rounded image
-    # magick border_mask.png \
-    #   rounded_image.png -geometry +2+2 -composite \
-    #   rounded_image_with_boder.png
+      magick \( xc:red xc:'rgb(170, 54, 206)' +append \) \
+        \( xc:'rgb(187, 224, 122)' xc:'rgb(231, 83, 219)' +append \) -append \
+        -size ${width_bg}x${height_bg} xc: +swap  -fx 'v.p{i/(w-1),j/(h-1)}' \
+        gradient_bg.jpg
+    }
 
-    # make shadow 1
-    magick rounded_image.png \
-      \( +clone -background "rgb(0, 0, 0)" -shadow 60x7+3+3 \) \
-      +swap -background none -layers merge +repage \
-      tmp_shadow1.png
+    function Join_image() {
+      echo "Join image with background..."
 
-    # make shadow 2
-    magick tmp_shadow1.png \
-      \( +clone -background "rgb(0, 0, 0)" -shadow 60x7-3+3 \) \
-      +swap -background none -layers merge +repage \
-      rounded_image_with_boder_shadowed.png
+      magick gradient_bg.jpg \
+        \( rounded_image_with_boder_shadowed.png \) -gravity center -composite \
+        output.png
+    }
 
-    # make background gradient
-    magick \( xc:red xc:'rgb(170, 54, 206)' +append \) \
-      \( xc:'rgb(187, 224, 122)' xc:'rgb(231, 83, 219)' +append \) -append \
-      -size ${width_bg}x${height_bg} xc: +swap  -fx 'v.p{i/(w-1),j/(h-1)}' \
-      gradient_bg.jpg
+    function Add_text() {
+      echo "Adding text..."
 
-    # make final image
-    magick gradient_bg.jpg \
-  \( rounded_image_with_boder_shadowed.png \) -gravity center -composite \
-  output.png
+      magick output.png \
+        -font $FONT \
+        -gravity north \
+        -pointsize 15 \
+        -fill "#ffffff" -stroke "#000000" -strokewidth 2 \
+        -annotate +0+30 "Screenshot Keren!" \
+        output_with_text.png
+    }
 
-  magick output.png \
-  -font ~/.fonts/MyCoolFont.ttf \
-  -gravity north \
-  -pointsize 15 \
-  -fill "#ffffff" -stroke "#000000" -strokewidth 2 \
-  -annotate +0+30 "Screenshot Keren!" \
-  output_with_text.png
+    function Save_image() {
+      echo "Saving image..."
 
-    new_dir=$WATCH_DIR/good
-    if [ ! -d "$new_dir" ]; then
-      mkdir $new_dir
-    fi
-    mv output_with_text.png $WATCH_DIR/good/$NEWNAME
-
-    # remove temp files
-    rm  gradient_bg.jpg tmp_shadow1.png border_redius_mask.png rounded_image.png 
+      new_dir=$WATCH_DIR/good
+      if [ ! -d "$new_dir" ]; then
+        mkdir $new_dir
+      fi
+      mv output_with_text.png $WATCH_DIR/good/$NEWNAME
+    }
+    Make_image_with_border_rounded
+    Make_shadow
+    Make_background
+    Join_image
+    Add_text
+    Save_image
 
     exit 1
   else
@@ -103,3 +106,4 @@ inotifywait -e create --format '%f' "$WATCH_DIR" | while read NEWFILE ; do
     exit 1
   fi
 done
+
